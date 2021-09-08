14a3c82adbb7917a67f648f9fbc269bb6f1a37b8
Consuelo Garrido Abedrapo <consuelogarrido.ab@gmail.com>
2021-09-08 15:14:44 -0300

    first commit

---
+++ app/models/variant.rb
@@ -0,0 +1,9 @@
+class Variant < ApplicationRecord
+  belongs_to :product
+  belongs_to :color
+  belongs_to :size
+
+  # variante unica
+  validates :product_id, uniqueness: {scope: %i[color_id size_id]}
+
+end
