88892b6bcaf18b94445b8cedeaf7e1b192949474
Consuelo Garrido Abedrapo <consuelogarrido.ab@gmail.com>
2021-09-09 12:33:26 -0300

    test unitarios

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,15 @@
 class Payment < ApplicationRecord
   belongs_to :order
   belongs_to :payment_method
+
+  def close!
+    ActiveRecord::Base.transaction do
+      complete
+      order.complete
+    end
+  end
+
+  def complete
+      update_attribute(:state, "completed")
+  end
 end
